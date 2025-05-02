c76d36802634a06af3347f07af067dfdfc67719d
miguelvalero <miguel.valero@upc.edu>
2025-05-02 10:42:39 +0200

    con RC_override

--- dronLink/tests/test_mov.py
+++ dronLink/tests/test_mov.py
@@ -5,13 +5,16 @@ from dronLink.Dron import Dron
 dron = Dron()
 connection_string = 'tcp:127.0.0.1:5763'
 baud = 115200
+connection_string = 'com8'
+baud = 57600
+
 
 print ('voy a conectarme')
 dron.connect (connection_string, baud)
 print ('conectado')
 dron.arm()
-dron.takeOff (5)
-print ('Voy al Norte 20 metros')
+dron.takeOff (2)
+'''print ('Voy al Norte 20 metros')
 dron.move_distance ('North', 20)
 print ('Voy al oeste 40 metros')
 dron.move_distance ('West', 40)
@@ -19,13 +22,14 @@ print ('me muevo arriba 5 metros')
 dron.move_distance ('Up', 5)
 print ('Voy atras 20 metros')
 dron.move_distance ('Back', 20)
-print ('me muevo hacia adelante 50 metros')
-dron.move_distance ('Forward', 50)
+print ('me muevo hacia adelante 50 metros')'''
+dron.move_distance ('Forward', 1)
 print ('cambio de heading')
 dron.changeHeading(275)
-print ('me muevo a la izquierda 40 metros')
+'''print ('me muevo a la izquierda 40 metros')
 dron.move_distance ('Left', 40)
-
-dron.RTL()
+'''
+#dron.RTL()
+dron.Land()
 dron.disconnect()
 
--- dronLink/tests/test_telemetry_info.py
+++ dronLink/tests/test_telemetry_info.py
@@ -3,17 +3,18 @@ import time
 from dronLink.Dron import Dron
 dron = Dron()
 connection_string = 'tcp:127.0.0.1:5763'
-connection_string = 'com13'
+connection_string = 'com21'
 
 baud = 115200
 baud = 4800
+baud = 57600
 dron.connect(connection_string, baud)
 print ('conectado')
 def procesarTelemetria (telemetryInfo ):
     print ('global:', telemetryInfo)
 def procesarTelemetriaLocal (telemetryInfo ):
     print ('local:' , telemetryInfo)
-dron.send_telemetry_info(procesarTelemetria)
+#dron.send_telemetry_info(procesarTelemetria)
 time.sleep (10)
 dron.send_local_telemetry_info(procesarTelemetriaLocal)
 while True:

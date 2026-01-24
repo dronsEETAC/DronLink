82f266669ca549be78cac453b6f56540624e7cf8
demoGit123456 <demogitdemogit@gmail.com>
2026-01-24 11:12:25 +0100

    arreglo armar

---
+++ demostradores/lanzar.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Users\Miguel\Documents\Miguel\DEE\DronLink\.venv\Scripts\pythonw.exe" "C:\Users\Miguel\Documents\Miguel\DEE\DronLink\demostradores\DahboardDirecto.py"
+pause
--- dronLink/modules/dron_arm.py
+++ dronLink/modules/dron_arm.py
@@ -1,5 +1,8 @@
 import logging
 import threading
+import time
+from os import times_result
+
 from pymavlink import mavutil
 
 def setFlightMode (self,mode):
@@ -13,7 +16,7 @@ def setFlightMode (self,mode):
     if self.verbose:
         logging.info("Pongo el dron en modo %s", str(mode))
 
-def _arm(self, callback=None, params = None):
+def _arm2(self, callback=None, params = None):
     self.state = "arming"
     self.setFlightMode ('GUIDED')
     self.vehicle.mav.command_long_send(self.vehicle.target_system, self.vehicle.target_component,
@@ -24,6 +27,42 @@ def _arm(self, callback=None, params = None):
 
     if self.verbose:
         logging.info("Dron armado")
+
+    self.state = "armed"
+    if callback != None:
+        if self.id == None:
+            if params == None:
+                callback()
+            else:
+                callback(params)
+        else:
+            if params == None:
+                callback(self.id)
+            else:
+                callback(self.id, params)
+
+def _arm(self, callback=None, params = None):
+    self.state = "arming"
+    self.setFlightMode ('GUIDED')
+    self.vehicle.mav.command_long_send(self.vehicle.target_system, self.vehicle.target_component,
+                                         mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0, 1, 0, 0, 0, 0, 0, 0)
+    msg = self.message_handler.wait_for_message('COMMAND_ACK', timeout=3)
+    timeout = 10
+    start_time = time.time()
+    armed = False
+    while time.time() - start_time < timeout:
+        if self.vehicle.motors_armed():
+            armed = True
+            if self.verbose:
+                logging.info("Dron armado")
+            break
+        time.sleep(0.1)
+    if not armed:
+        self.state = "connected"
+
+        if self.verbose:
+            logging.info("El dron no se ha armado: timeout superado")
+        return
 
     self.state = "armed"
     if callback != None:
